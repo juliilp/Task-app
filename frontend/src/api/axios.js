f1f35047edcd3ba2377825f7a569c0f5932139a4
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-07-30 15:51:57 -0300

    cambié la url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:3000",
+  baseURL: "https://task-app-production.up.railway.app/",
 });
 
 export default instance;
